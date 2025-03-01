7fb2025753c9a522a36ffab357e659ef3b9cdb24
Matt Topol <zotthewizard@gmail.com>
2025-03-01 16:28:19 -0500

    add missing RecoverError

--- internal/utils.go
+++ internal/utils.go
@@ -18,6 +18,7 @@
 package internal
 
 import (
+	"fmt"
 	"io"
 )
 
@@ -31,4 +32,15 @@ func (w *CountingWriter) Write(p []byte) (int, error) {
 	w.Count += int64(n)
 
 	return n, err
+}
+
+func RecoverError(err *error) {
+	if r := recover(); r != nil {
+		switch e := r.(type) {
+		case string:
+			*err = fmt.Errorf("error encountered during arrow schema visitor: %s", e)
+		case error:
+			*err = fmt.Errorf("error encountered during arrow schema visitor: %w", e)
+		}
+	}
 }
